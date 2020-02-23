888c65bec63da1c77dd93dfdcb10539f70b825c9
Frozen Feelings <kiprutohcollo@gmail.com>
2020-02-23 13:34:55 +0300

    chaged icons headings

---
+++ js/index.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function () {
+    $('#toggle').click(function () {
+        $('#p1').show();
+    });
+    $('#toggle').click(function () {
+        $('#toggle').hide();
+    });
+
+    $('#p1').click(function () {
+        $('#toggle').show();
+    });
+    $('#p1').click(function () {
+        $('#p1').hide();
+    }); 
+    $('#toggle2').click(function () {
+        $('#p2').show();
+    });
+    $('#toggle2').click(function () {
+        $('#toggle2').hide();
+    });
+
+    $('#p2').click(function () {
+        $('#toggle2').show();
+    });
+    $('#p2').click(function () {
+        $('#p2').hide();
+    });  
+    $('#toggle3').click(function () {
+        $('#p3').show();
+    });
+    $('#toggle3').click(function () {
+        $('#toggle3').hide();
+    });
+
+    $('#p3').click(function () {
+        $('#toggle3').show();
+    });
+    $('#p3').click(function () {
+        $('#p3').hide();
+    });
